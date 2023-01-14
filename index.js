2ade393d16e9817ec74dc6914787c8ada7cf4e8d
Tirsa St. Fort <lifelovelori@gmail.com>
2023-01-13 20:19:40 -0800

    Completed lab.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'joe'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'jane'
+}
